c440d9d9633fb746f186bf179dc9b99013444273
sigmak <dwfree74@gmail.com>
2015-12-14 01:06:09 +0900

    ver 0.0.1 버전 체크 출력

---
+++ FPP.cpp
@@ -0,0 +1,16 @@
+// from bpp.cpp
+#include "fpp.h"
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "\nF++ compiler version " << version.major << '.'
+		<< version.minor << '.' << version.release << '\n';
+	cout << '\n';
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
---
+++ FPP.h
@@ -0,0 +1,5 @@
+// from bpp.h
+const struct
+{
+	int major, minor, release;
+} version = { 0, 0, 1 };
